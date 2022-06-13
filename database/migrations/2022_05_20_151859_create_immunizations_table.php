d3e6f6b54b7d6a34ac4170e51f7fe6960e4e60c2
iyus yusa <iyusyusa880@gmail.com>
2022-06-13 20:48:45 +0700

    membuat database migrations imunisasi

---
+++ database/migrations/2022_05_20_151859_create_immunizations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImmunizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('immunizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age');
+            $table->text('address');
+            $table->string('birth_place');
+            $table->date('birth_date');
+            $table->string('weight');
+            $table->string('temperature');
+            $table->text('description');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('immunizations');
+    }
+}
