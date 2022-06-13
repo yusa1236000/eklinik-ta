fd6c03d34b22f0632fbe45a49b47749171305dcf
iyus yusa <iyusyusa880@gmail.com>
2022-06-13 19:58:04 +0700

    membuat API service

---
+++ app/Http/Controllers/API/ServiceController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Service;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $service = Service::query()->get();
+        if (count($service) == 0) {
+            return response()->json(["message" => "Patient Kosong"]);
+            }
+            return response()->json($service);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+        $service = Service::create($request->all());
+        return response()->json($service);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $service = Service::find($id);
+
+        $request->validate([
+            'name' => 'required'
+        ]);
+        $service->update([
+            'name' => $request->name
+        ]);
+        return response()->json($service);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $service = Service::find($id)->delete();
+        return response()->json($service);
+    }
+}
